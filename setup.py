b86f8a2a2f04a5fc336c88cee9e8858af1290967
kaze <zsoltkormany@gmail.com>
2018-05-25 21:05:28 +0200

    update setup.py
    Remove versioning of django-guardian dependency, remove old Django and Python versions.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except IOError:
 
 install_requires = [
     'easy_thumbnails',
-    'django-guardian<=1.4.1',
+    'django-guardian',
     'html2text==2014.12.29'
 ]
 
@@ -41,23 +41,12 @@ setup(name='django-userena',
           'Development Status :: 4 - Beta',
           'Environment :: Web Environment',
           'Framework :: Django',
-          'Framework :: Django :: 1.5',
-          'Framework :: Django :: 1.6',
-          'Framework :: Django :: 1.7',
-          'Framework :: Django :: 1.8',
-          'Framework :: Django :: 1.9',
+          'Framework :: Django :: 1.11',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Topic :: Utilities'
       ],
       )
